388f5f19010d47589299e245548abfdc01bb3fbf
beartato327 <rcapurro@unomaha.edu>
2020-08-31 18:50:17 -0500

    update

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+
+admin.site.register(Client, ClientAdmin)
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.views.generic import ListView, DetailView
+from .models import models
+from .models import Client
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class ClientListView(LoginRequiredMixin,ListView):
+    model = Client
+    template_name = 'client_list.html'
+
+class ClientUpdateView(LoginRequiredMixin,UpdateView):
+    model = Client
+    fields = ('name', 'notes', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone', 'acct_number')
+    template_name = 'client_edit.html'
+
+class ClientDetailView(LoginRequiredMixin,DetailView):
+    model = Client
+    template_name = 'client_detail.html'
+    login_url = 'login'
+
+class ClientDeleteView(LoginRequiredMixin,DeleteView):
+    model = Client
+    template_name = 'client_delete.html'
+    success_url = reverse_lazy('client_list')
+
+class ClientCreateView(LoginRequiredMixin,CreateView):
+    model = Client
+    template_name = 'client_new.html'
+    fields = ('name', 'notes', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone', 'acct_number')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
