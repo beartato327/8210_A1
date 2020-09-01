c0e3f22cb67b4be84d14e108fa6c059c7a77c31e
beartato327 <rcapurro@unomaha.edu>
2020-09-01 15:53:03 -0500

    finished tutorial

--- clients/admin.py
+++ clients/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Client
+from .models import Client, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
 
 class ClientAdmin(admin.ModelAdmin):
-    model = Client
+    inlines = [
+        CommentInline,
+    ]
+
 
 admin.site.register(Client, ClientAdmin)
+admin.site.register(Comment)
+
